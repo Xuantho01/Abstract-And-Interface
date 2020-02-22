811faa8a97e9c545fa006889bfb2b9394ec38136
Xuantho01 <hoxuantho95@gmail.com>
2020-02-22 22:33:22 +0700

    add file

---
+++ Animal And Interface Edible/src/Chicken/Chicken.java
@@ -0,0 +1,16 @@
+package Chicken;
+import edible.Edible;
+import Animal.Animal;
+
+public class Chicken extends Animal implements Edible {
+
+    @Override
+    public String makeSound() {
+        return "Chicken: cluck-cluck!";
+    }
+
+    @Override
+    public String howToEat() {
+        return "could be fried";
+    }
+}
---
+++ Animal And Interface Edible/src/Tiger/Tiger.java
@@ -0,0 +1,10 @@
+package Tiger;
+
+import Animal.Animal;
+
+public class Tiger extends Animal {
+    @Override
+    public String makeSound() {
+        return "Tiger: roarrrrr!";
+    }
+}
